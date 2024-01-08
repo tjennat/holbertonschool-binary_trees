e1c13a5b941c3fa82d5f9ec2795432ff66a7e484
tjennat <theo.jennat98@gmail.com>
2024-01-08 20:14:11 +0100

    fixing commentary not close properly

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,34 +5,34 @@
  * @parent: parent of the new node
  * @value: value of the new right node
  * Return: pointer to the new node
- * /
+ */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = parent->right;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
 
-    if (parent->right != NULL)
-    {
-        parent->right->parent = new_node;
-    }
-    parent->right = new_node;
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
