6b143569ec51b803c2af3616d969ae51339495cd
Spacefox95 <nathan.raynal@gmail.com>
2024-01-09 00:31:51 -0800

    Betty_1_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,30 +8,30 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = parent->left;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = parent->left;
+	new_node->right = NULL;
 
-    if (parent->left != NULL)
-    {
-        parent->left->parent = new_node;
-    }
-    
-    parent->left = new_node;
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+	}
 
-    return (new_node);
+	parent->left = new_node;
+
+	return (new_node);
 }
