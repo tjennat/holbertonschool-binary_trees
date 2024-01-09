d41f1b87427e7409d0f1324c250fe12b43138742
Spacefox95 <nathan.raynal@gmail.com>
2024-01-09 13:59:12 +0100

    Betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
+ * binary_tree_is_root - check if the node is a root
+ * @node: the node to check
+ * Return: 1 if root, else 0
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
