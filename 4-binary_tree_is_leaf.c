996506493226ecf3feafc802614b2a08108385f2
Spacefox95 <nathan.raynal@gmail.com>
2024-01-09 01:02:25 -0800

    Betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,31 +4,31 @@
  * binary_tree_is_leaf -  check if a node is a leaf
  * @node: the node to check
  * Return: 1 if node, else 0
-*/
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return (0);
-    }
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
 
-    new_node->parent = node->parent;
-    new_node->left = node->left;
-    new_node->right = node->right;
+	new_node->parent = node->parent;
+	new_node->left = node->left;
+	new_node->right = node->right;
 
-    if (new_node->left == NULL && new_node->right == NULL)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (new_node->left == NULL && new_node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
