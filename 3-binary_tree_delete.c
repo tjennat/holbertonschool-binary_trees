a81efc66fc867fd3abaf4def69df3a19f430a2cc
Spacefox95 <nathan.raynal@gmail.com>
2024-01-09 00:39:29 -0800

    Betty_3-free

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,19 +2,20 @@
 
 /**
  * binary_tree_delete - free the binary tree
+ * @tree: the tree to delete
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return;
-    }
-    else
-    {
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
-        free(tree);
-	tree = NULL;
-    }
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+		tree = NULL;
+	}
 }
