4e2bcd9de397dcd9b22db209249cf321dc36a5d8
tjennat <theo.jennat98@gmail.com>
2024-01-10 14:03:09 +0100

    correcting betty + header + some parts of the code

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,22 +1,20 @@
-#include <stdlib.h>
-
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - function that check if tree is full
+ * @tree: the binary tree
+ * Return: tree full or 0
+ */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 1;
+		return (1);
 
 	if ((tree->left) && (tree->right))
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-	return 0;
+	return (0);
 }
