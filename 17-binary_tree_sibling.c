762ddf62d774041b12adafb983c51f7255bffc43
Spacefox95 <nathan.raynal@gmail.com>
2024-01-11 13:11:38 +0100

    fixgithub

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,30 +2,6 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node
- * @node: the node to test
- * Return: pointer to the node find
-*/
-
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-	if (!node || !node->parent)
-	{
-		return (NULL);
-	}
-
-	if (node->parent->left && node->parent->right)
-	{
-		if (node == node->parent->left)
-			return (node->parent->right);
-		if (node == node->parent->right)
-			return (node->parent->left);
-	}
-	return (NULL);
-}
-#include "binary_trees.h"
-
-/**
- * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
  * Return: pointer to the sibling node
  */
