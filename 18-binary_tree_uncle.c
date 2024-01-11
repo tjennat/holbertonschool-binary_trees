0f8ed6e1cf681c5b17a6f15f5261c03848c503dd
Spacefox95 <nathan.raynal@gmail.com>
2024-01-11 13:32:58 +0100

    final

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent || !node->parent->parent )
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 	if (node->parent->parent->left && node->parent->parent->right)
 	{
